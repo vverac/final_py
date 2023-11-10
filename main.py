c8e1db09fc2010f3339ab3aefcfe634e916c84d4
vverac <viverace@gmail.com>
2023-11-09 21:54:32 -0300

    consulta a la db

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI, Request, Form, status
+from fastapi import FastAPI, Request, Form, status, Depends
 # libreria para usar cualquier motor de plantillas
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
-
 import models
 from database import engine, sessionlocal
 # SQLAlchemy para manejar operaciones CRUD en un modelo de usuario, para interactuar con la base de datos.
 from sqlalchemy.orm import Session
+# modulo para servir archivos estaticos
+from fastapi.staticfiles import StaticFiles
 
 models.Base.metadata.create_all(bind=engine)
 # indicamos desdedonde importaremos nuestros templates, para ello crearemos la carpeta templates
@@ -16,7 +17,11 @@ templates = Jinja2Templates(directory='./templates')
 app = FastAPI()
 
 
-# funcion que crea una nueva sesión de base de dato
+# app.mount('/static', StaticFiles(directory='static'), name='static')
+
+# funcion que crea una nueva sesión de base de datos
+
+
 def get_db():
     db = sessionlocal()
     try:
@@ -26,8 +31,9 @@ def get_db():
 
 
 @app.get('/')
-async def home(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+async def home(request: Request, db: Session = Depends(get_db)):
+    users = db.query(models.User).order_by(models.User.id.desc())
+    return templates.TemplateResponse('index.html', {'request': request, 'users': users})
 
 
 @app.get('/addnew')
@@ -36,6 +42,9 @@ async def add(request: Request):
 
 
 @app.post('/add')
-async def add(request: Request, tarea: str = Form(...)):
+async def add(request: Request, tarea: str = Form(...), db: Session = Depends(get_db)):
     print(tarea)
+    users = models.User(tarea=tarea)
+    db.add(users)
+    db.commit()
     return RedirectResponse(url=app.url_path_for('home'), status_code=status.HTTP_303_SEE_OTHER)
--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(150))
+    tarea = Column(String(150))
 
     def __repr__(self):
         return '<User %r>' % (self.id)
